c469e3f7e3f21f1069e66764c1ef4267f06374c6
zakaria-root <elmourtazakezia@gmail.com>
2021-09-25 21:24:12 +0100

    add the discription

--- App.js
+++ App.js
@@ -20,43 +20,43 @@ export default function App() {
   const data = [
     {id: 1,
     name: "P R E Y",
-    disc: "Prey est un jeu de type FPS, orienté action, jouable en solo. Le joueur se réveille au cours de l'année 2032, à bord de la station spatiale Talos I, en orbite autour de la lune. ",
+    disc: "Prey est un jeu de type FPS, orienté action, jouable en solo. ",
     uri: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTGfDwTQVq0HdMjiF9mzEoJsvrhXc3qcJVE1EtzRq4Wk9w7Ox5T"},
     {id: 2,
-    name: "Sweet Girl - film 2021",
-    disc: " Un homme jure de venger les responsables de la mort de sa femme, tout en protégeant sa fille, la seule famille qu'il lui reste. ",
+    name: "Sweet Girl",
+    disc: "Un homme jure de venger les responsables de la mort de sa femme. ",
     uri: "https://fr.web.img2.acsta.net/c_310_420/pictures/21/08/04/12/34/5035523.jpg"},
     {id: 3,
-    name: "The Call - Film (2020)",
-    disc: "Seo-yeon reçoit de plus en plus d’appels étranges ne semblant pas lui être destinés provenant d’une dénommée Yeong-sook. ",
+    name: "The Call",
+    disc: "Seo-yeon reçoit de plus en plus d’appels étranges ne semblant. ",
     uri: "https://media.senscritique.com/media/000019776906/source_big/The_Call.jpg"},
     {id: 4,
     name: "SAS: Red Noticf",
-    disc: "Intitulé par Ruby Rose, ce thriller d’action britannique raconte l’histoire d’une petite armée de criminels qui tentent leur plus grand vol avec un train qui s’enfonce profondément dans la Manche reliant l’Angleterre et la France.",
+    disc: "Intitulé par Ruby Rose, ce thriller d’action britannique.",
     uri: "https://www.cimaton.org/wp-content/uploads/2021/03/SAS-Red-Notice-2021.jpg"},
     {id: 5,
-    name: "Tyler Rake - film 2020",
-    disc: "Tyler Rake est un mercenaire intrépide qui travaille dans l'ombre. Alors qu'il n'a plus rien à perdre, il est chargé par un puissant caïd mafieux, pour l'heure incarcéré.",
+    name: "Tyler Rake",
+    disc: "Tyler Rake est un mercenaire intrépide qui travaille dans l'ombre.",
     uri: "https://fr.web.img6.acsta.net/pictures/20/03/31/09/22/0259222.jpg"},
     {id: 6,
-    name: "Le Roi - film 2019",
-    disc: "Hal, jeune prince rebelle, tourne le dos à la royauté pour vivre auprès du peuple. Mais à la mort de son père, le tyrannique Henri IV d'Angleterre, Hal ne peut plus échapper au destin qu'il tentait de fuir et est couronné roi à son tour. ",
+    name: "Le Roi",
+    disc: "Hal, jeune prince rebelle, tourne le dos à la royauté. ",
     uri: "https://fr.web.img5.acsta.net/pictures/19/08/22/09/22/0020378.jpg"},
     {id: 7,
     name: "V E N O M",
-    disc: "Depuis la sortie du premier volet de Venom, les fans du MCU espèrent voir un jour sur le grand écran un affrontement entre Venom et Spider-Man, son ennemi juré. Lors d’une interview, Ruben Fleischer, le réalisateur du film, n’avait pas écarté cette possibilité.",
+    disc: "Depuis la sortie du premier volet de Venom, les fans du MCU espèrent.",
     uri: "https://www.aleqt.com/sites/default/files/styles/scale_660/public/rbitem/2018/09/13/903721-1774303301.jpg?itok=dcb_TBfJ"},
     {id: 8,
-    name: "The Old Guard - film 2020",
-    disc: "Une petite bande soudée de mercenaires immortels, dirigée par la redoutable Andy, se bat depuis des siècles pour protéger les humains. Mais tandis que le groupe est engagé pour une mission des plus périlleuses.",
+    name: "The Old Guard",
+    disc: "Une petite bande soudée de mercenaires immortels.",
     uri: "https://fr.web.img4.acsta.net/pictures/20/05/26/09/44/5988886.jpg"},
     {id: 9,
     name: "Bird Box eBook by Josh Malerman",
-    disc: "Written with the narrative tension of The Road and the exquisite terror of classic Stephen King, Bird Box is a propulsive, edge-of-your-seat horror thriller, set in an apocalyptic near-future world—a masterpiece of suspense from the brilliantly imaginative Josh Malerman.",
+    disc: "Written with the narrative tension of The Road and the exquisite.",
     uri: "https://kbimages1-a.akamaihd.net/2352a6df-3541-4474-9a9d-44fce5c9f3b8/1200/1200/False/bird-box-1.jpg"},
     {id: 10,
-    name: "The Kissing Booth 2 - film 2020",
-    disc: " Tout en postulant à plusieurs universités, Elle jongle entre sa relation à distance avec Noah, l'évolution de son amitié avec Lee, et son béguin pour un petit nouveau. ",
+    name: "The Kissing Booth 2",
+    disc: "Tout en postulant à plusieurs universités. ",
     uri: "https://fr.web.img6.acsta.net/pictures/20/05/22/09/26/0527531.jpg"},
   ]
   return (
@@ -70,19 +70,25 @@ export default function App() {
       }}
       renderItem={({item}) => {
         return(
-          <View>
+          <View style={{ 
+            width:width_image, 
+            height: height_image, 
+            margin:20,
+            borderRadius: 20,
+            backgroundColor: 'red'
+          }}>
             <Image 
             source={{ uri : item.uri }}
             
             style={{ 
               width:width_image, 
               height: height_image, 
-              margin:20,
+              // margin:20,
               borderRadius: 20
             }}
             />
             
-            <View style={{ alignItems: 'center' }}>
+            <View style={{ alignItems: 'center', marginVertical: 15 }}>
               <Text
           style={{ 
             fontWeight: 'bold',
@@ -90,6 +96,11 @@ export default function App() {
           }}
           >{item.name}
           </Text>
+            </View>
+            <View style={{ paddingLeft: 10 }}>
+              <Text style={{ alignItems: 'center' }}>
+              {item.disc}
+              </Text>
             </View>
             
           </View>
